9cff84e9e243ab9217b1f04855608a9f583ea613
Mr.Liu <liuyx.android@gmail.com>
2019-08-29 11:28:19 +0800

    Update ZzyMosSso.js

--- www/ZzyMosSso.js
+++ www/ZzyMosSso.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 /**
  * 单点中获取对应key的值
- * @param mos_username  mos空间单点用户名
- * @param mos_token   mos空间单点token
- * @param mos_password    mos空间单点密码 
+ * @param key mos_username  mos空间单点用户名  mos_token   mos空间单点token  mos_password    mos空间单点密码 
+ * @param success 成功调用回调
+ * @param error  成功失败调用回调
  */
 exports.pullFromGlobal = function (key, success, error) {
     exec(success, error, 'ZzyMosSso', 'pullFromGlobal', [key]);
