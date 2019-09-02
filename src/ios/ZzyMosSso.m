d956899c2957531d22a1ba8559a03c28fd6f208d
Mr.Liu <liuyx.android@gmail.com>
2019-09-02 23:23:40 +0800

    Update ZzyMosSso.m

--- src/ios/ZzyMosSso.m
+++ src/ios/ZzyMosSso.m
@@ -1,7 +1,7 @@
 /********* ZzyMosSso.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
-
+#import <UUPortalToolSDK/UUPortalToolSDK.h>
 @interface ZzyMosSso : CDVPlugin {
   // Member variables go here.
 }
@@ -15,9 +15,29 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
+    // get mos data
+    // 定义获取数据key
+    NSString* mos_username = [NSString stringWithFormat:@"mos_username"];
+    NSString* mos_token = [NSString stringWithFormat:@"mos_token"];
+    NSString* mos_password = [NSString stringWithFormat:@"mos_password"];
+    // 判断传入的key值是否有效
     if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        // 默认返回为Return a empty result
+        NSString *result = @"Return a empty result";
+        if ([echo isEqualToString:mos_username]) {
+           //通過ＳＤＫ 獲取username
+           result = [UUPortalToolSDKManager userName];
+           NSLog(@"mos_username");
+        }else if ([echo isEqualToString:mos_token]){
+            //通過ＳＤＫ 獲取token
+           result = [UUPortalToolSDKManager token];
+           NSLog(@"mos_token");
+        }else if ([echo isEqualToString:mos_password]){
+            //通過ＳＤＫ 獲取password
+           result = [UUPortalToolSDKManager password];
+           NSLog(@"mos_password");
+        }
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
